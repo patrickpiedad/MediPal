a37a6d1f0185983dbafe9893844a8bf9606156eb
patrickpiedad <patrickpiedad@gmail.com>
2024-08-16 19:37:29 +0200

    Initial creation of MAUI Blazor Hybrid project for MediPal

---
+++ MediPal/MediPal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MediPal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MediPal/MediPal/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MediPal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
