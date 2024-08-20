1dbd426a62e71b0521f8bab0e0972c0b5057cebd
patrickpiedad <patrickpiedad@gmail.com>
2024-08-20 21:26:07 +0200

    Immediate post-migration build / database update. Symptom tracker functioning with SQL-Lite integration.

---
+++ MediPal/MediPal/Migrations/20240820192414_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediPal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SymptomList",
+                columns: table => new
+                {
+                    SymptomID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Symptom = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    PainLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    Activity = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SymptomList", x => x.SymptomID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SymptomList");
+        }
+    }
+}
