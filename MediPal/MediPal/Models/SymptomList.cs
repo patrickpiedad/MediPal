8696109400a9e09f9c495b2c9f062f7dc6f26e33
patrickpiedad <patrickpiedad@gmail.com>
2024-08-20 19:57:16 +0200

    Adjusted symptom list class and public properties. This commit is pre-database creation.

--- MediPal/MediPal/Models/SymptomList.cs
+++ MediPal/MediPal/Models/SymptomList.cs
@@ -4,18 +4,11 @@ namespace MediPal.Models
 {
     public class SymptomList
     {
-
-        private int? _symptomID;
-        private string? _symptom;
-        private DateOnly? _date;
-        private int? _painLevel;
-        private string? _activity;
-
         [Key]
-        public int? SymptomID { get => _symptomID; set => _symptomID=value; }
-        public string? Symptom { get => _symptom; set => _symptom=value; }
-        public DateOnly? Date { get => _date; set => _date=value; }
-        public int? PainLevel { get => _painLevel; set => _painLevel=value; }
-        public string? Activity { get => _activity; set => _activity=value; }
+        public int SymptomID { get; set; }
+        public string? Symptom { get; set; }
+        public DateOnly Date { get; set; }
+        public int PainLevel { get; set; }
+        public string? Activity { get; set; }
     }
 }
