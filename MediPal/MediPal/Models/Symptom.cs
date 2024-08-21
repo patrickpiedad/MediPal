f03c0875168be48f38a46e95e02c4d483803a9d1
patrickpiedad <patrickpiedad@gmail.com>
2024-08-22 01:33:39 +0200

    Stable. SymptomList refactored to Symptom for clarification. Using manual list for data storage. This commit is PRE-DATABASE reintegration.

--- MediPal/MediPal/Models/Symptom.cs
+++ MediPal/MediPal/Models/Symptom.cs
@@ -2,19 +2,18 @@
 
 namespace MediPal.Models
 {
-    public class SymptomList
+    public class Symptom
     {
         [Key]
         public int SymptomID { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "Symptom description cannot exceed 100 characters.")]
-        public string? Symptom { get; set; }
+        public string? SymptomName { get; set; }
 
         [Required]
         public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         //public DateTime Date { get; set; } = DateTime.Today;
-
 
         [Required]
         [Range(0, 10, ErrorMessage = "Pain level must be between 0 and 10.")]
