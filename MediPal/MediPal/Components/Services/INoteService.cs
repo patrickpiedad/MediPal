4c50bc7d6ff8f7af0fb8133e444a4c80b5e9d5a6
patrickpiedad <patrickpiedad@gmail.com>
2024-10-09 21:46:48 +0200

    feat. updated doctor dashboard to display patient information and notes in addition to symptoms. UI updates to be more readable.

--- MediPal/MediPal/Components/Services/INoteService.cs
+++ MediPal/MediPal/Components/Services/INoteService.cs
@@ -6,6 +6,7 @@ namespace MediPal.Components.Services
     {
         Task<List<Note>> GetAllNotesAsync();
         Task<List<Note>> GetNotesByUserIdAsync(string userId); // This method gets all notes by ASPNETUserId, instead of just all notes
+        Task<List<Note>> GetNotesByPatientIdAsync(string patientId); // This method gets all notes based on patientId
         Task<Note> GetNoteByIdAsync(int id);
         Task AddNoteAsync(Note note);
         Task UpdateNoteAsync(Note note, int id);
--- MediPal/MediPal/Components/Services/ISymptomService.cs
+++ MediPal/MediPal/Components/Services/ISymptomService.cs
@@ -8,7 +8,6 @@ namespace MediPal.Components.Services
         Task<List<Symptom>> GetSymptomsByUserIdAsync(string userId); // This method gets all symptoms by ASPNETUserId, instead of just all symptoms
         Task<List<Symptom>> GetSymptomsByPatientIdAsync(string patientId); // This method gets all symptoms based on patientId
         Task<Symptom> GetSymptomByIdAsync(int id);
-
         Task AddSymptomAsync(Symptom symptom);
         Task UpdateSymptomAsync(Symptom symptom, int id);
         Task DeleteSymptomAsync(int id, string userId);
